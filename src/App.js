c5bb69eb8ffb851f5d33ab00d52a6b46ffe496af
Sam <samnewell91@gmail.com>
2023-10-23 17:17:51 +0100

    emoji library app
    (forgot to update repo as I went along)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import emojiList from "./emojiLibrary";
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Input searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <EmojiLibrary emojiList={emojiList} searchQuery={searchQuery} />
     </div>
   );
+}
+
+function Header() {
+  return (
+    <header>
+      <h1>Emoji Library</h1>
+    </header>
+  );
+}
+
+function Input({ searchQuery, setSearchQuery }) {
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search for an Emoji?"
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      ></input>
+    </div>
+  );
+}
+
+function EmojiLibrary({ emojiList, searchQuery }) {
+  const filteredEmojis = emojiList.filter((emoji) =>
+    emoji.keywords.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return searchQuery ? (
+    <div className="emoji-list">
+      {filteredEmojis.map((emoji, index) => (
+        <Emoji key={index} symbol={emoji.symbol} keywords={emoji.keywords} />
+      ))}
+    </div>
+  ) : (
+    <div className="emoji-list">
+      {emojiList.map((emoji, index) => (
+        <Emoji key={index} symbol={emoji.symbol} keywords={emoji.keywords} />
+      ))}
+    </div>
+  );
+}
+
+function Emoji({ symbol }) {
+  return <div className="emoji">{symbol}</div>;
 }
 
 export default App;
